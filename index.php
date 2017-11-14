af094c34da3ac012c73386350eee1cb8ab64054e
Matthew J Valdez <mjv32@afsaccess1.njit.edu>
2017-11-14 13:37:21 -0500

    added findAll() method

--- index.php
+++ index.php
@@ -45,4 +45,25 @@ class dbConn
 	}
 }
 
+class collection
+{
+	static public function create()
+	{
+		$model = new static::$modelName;
+		return $model;
+	}
 
+	static public function findAll()
+	{
+		$db = dbConn::getConnection();
+		$tableName = get_called_class()
+		$sql = 'SELECT * FROM ' . $tableName;
+		$statement = $db->prepare($sql);
+		$statement->execute();
+		$class = static::$modelName;
+		$statement->setFetchMode(PDO::FETCH_CLASS, $class);
+		$recordsSet = $statement->fetchAll();
+		return $recordsSet;
+	}
+
+	
